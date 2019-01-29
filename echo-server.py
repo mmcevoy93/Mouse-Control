87ee418ce0c29d4df3bacdcaed927040018f9a2c
Max McEvoy <mmcevoy@Maxs-iMac.local>
2019-01-29 11:27:57 -0700

    added mouse movement

--- echo-server.py
+++ echo-server.py
@@ -2,7 +2,7 @@ import socket
 import pyautogui
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+PORT = 8000       # Port to listen on (non-privileged ports are > 1023)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
