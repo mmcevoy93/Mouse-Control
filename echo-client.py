15b55425fe63ca411a222cf6c32e0925ff7c1184
Max McEvoy <mmcevoy@Maxs-iMac.local>
2019-01-29 11:10:27 -0700

    added mouse movement

--- echo-client.py
+++ echo-client.py
@@ -1,17 +1,14 @@
-#Test
-
 import socket
+import pyautogui
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
-
-
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+
 while True:
-    x = raw_input('XX: ')
-    s.sendall(bytes(x))
+    mouseXY = str(pyautogui.position()[0]) + "," + str(pyautogui.position()[1])
+    s.sendall(bytes(mouseXY))
     data = s.recv(1024)
 
     print('Received', repr(data))
--- echo-server.py
+++ echo-server.py
@@ -1,4 +1,5 @@
 import socket
+import pyautogui
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
@@ -7,11 +8,15 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(5)
 conn, addr = s.accept()
-
 print('Connected by', addr)
+size = pyautogui.size()
+
 while True:
+
     data = conn.recv(1024)
     if not data:
         break
     conn.sendall(data)
-    print(repr(data))
+    x = int(data.split(',')[0])
+    y = int(data.split(',')[0])
+    pyautogui.moveTo(x, y)
